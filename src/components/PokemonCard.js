09c1f41efce70a383d1550137e6d65cdff5326cd
monicaortiz20 <ortiznavajomonica@gmail.com>
2025-05-03 23:17:45 +0200

    added carousel

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,79 +1,48 @@
-import React, { useState } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import React from "react";
 import "../styles/pokemonCard.css";
+import bgCard from "../images/bg-nologo.png";
 
 function PokemonCard({ name, sprite, details }) {
-  const [showDetails, setShowDetails] = useState(false);
-
-  const handleCardClick = () => {
-    setShowDetails(true);
-  };
-
-  const handleCloseDetails = () => {
-    setShowDetails(false);
-  };
-
-  const sliderSettings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
-    <div>
-      <div onClick={handleCardClick} className="pokeCard">
+    <div className="pokeCardContainer">
+      <div className="pokeCard">
         {sprite && <img src={sprite} alt={name} />}
-        <h3>{name}</h3>
+        <div>
+          <h3>{name}</h3>
+          <p>{details.types.map((type) => type.type.name).join(", ")}</p>
+        </div>
       </div>
 
-      {showDetails && (
-        <div className="pokeDetailsCard">
-          <button onClick={handleCloseDetails} className="btnCard">
-            Close
-          </button>
-          <Slider {...sliderSettings}>
-            {/* Primera tarjeta: Base Experience y Abilities */}
-            <div className="carouselCard">
-              <h2>{details.name}</h2>
-              <p>
-                <strong>Base Experience:</strong> {details.base_experience}
-              </p>
-              <p>
-                <strong>Abilities:</strong>{" "}
-                {details.abilities
-                  .map((ability) => ability.ability.name)
-                  .join(", ")}
-              </p>
-            </div>
-
-            {/* Segunda tarjeta: Types */}
-            <div className="carouselCard">
-              <h2>{details.name}</h2>
-              <p>
-                <strong>Types:</strong>{" "}
-                {details.types.map((type) => type.type.name).join(", ")}
-              </p>
-            </div>
-
-            {/* Tercera tarjeta: Moves */}
-            <div className="carouselCard">
-              <h2>{details.name}</h2>
-              <p>
-                <strong>Moves:</strong>{" "}
-                {details.moves
-                  .slice(0, 5)
-                  .map((move) => move.move.name)
-                  .join(", ")}{" "}
-                (showing first 5)
-              </p>
-            </div>
-          </Slider>
+      <div className="container">
+        <input type="radio" name="slider" id="item-1" defaultChecked />
+        <input type="radio" name="slider" id="item-2" />
+        <input type="radio" name="slider" id="item-3" />
+        <div className="cards">
+          <label className="card" htmlFor="item-1" id="song-1">
+            <p>
+              <strong>Base Experience:</strong> {details.base_experience}
+            </p>
+          </label>
+          <label className="card" htmlFor="item-2" id="song-2">
+            <p>
+              <strong>Abilities:</strong>{" "}
+              {details.abilities
+                .map((ability) => ability.ability.name)
+                .join(", ")}
+            </p>
+          </label>
+          <label className="card" htmlFor="item-3" id="song-3">
+            <p>
+              <strong>Moves:</strong>{" "}
+              {details.moves
+                .slice(0, 5)
+                .map((move) => move.move.name)
+                .join(", ")}{" "}
+              (showing first 5)
+            </p>
+          </label>
         </div>
-      )}
+      </div>
     </div>
   );
 }
