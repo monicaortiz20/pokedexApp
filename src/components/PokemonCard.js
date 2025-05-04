83951a2ebfba3015430ecd74c024dfdf1053f9b2
monicaortiz20 <ortiznavajomonica@gmail.com>
2025-05-04 21:32:37 +0200

    added carousel && functionality

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -13,33 +13,37 @@ function PokemonCard({ name, sprite, details }) {
         </div>
       </div>
 
-      <div className="container">
+      <div className="containerData">
         <input type="radio" name="slider" id="item-1" defaultChecked />
         <input type="radio" name="slider" id="item-2" />
         <input type="radio" name="slider" id="item-3" />
         <div className="cards">
-          <label className="card" htmlFor="item-1" id="song-1">
+          <label className="cardData" htmlFor="item-1" id="song-1">
             <p>
-              <strong>Base Experience:</strong> {details.base_experience}
+              <strong>Base Experience:</strong>
             </p>
+            <span>{details.base_experience}</span>
           </label>
-          <label className="card" htmlFor="item-2" id="song-2">
+          <label className="cardData" htmlFor="item-2" id="song-2">
             <p>
               <strong>Abilities:</strong>{" "}
+            </p>
+            <span>
               {details.abilities
                 .map((ability) => ability.ability.name)
                 .join(", ")}
-            </p>
+            </span>
           </label>
-          <label className="card" htmlFor="item-3" id="song-3">
+          <label className="cardData" htmlFor="item-3" id="song-3">
             <p>
               <strong>Moves:</strong>{" "}
+            </p>
+            <span>
               {details.moves
                 .slice(0, 5)
                 .map((move) => move.move.name)
                 .join(", ")}{" "}
-              (showing first 5)
-            </p>
+            </span>
           </label>
         </div>
       </div>
